6ef53bb5af46132d0e0437930838831a825232b0
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-13 21:11:26 +0530

    push 1.3

--- blog-app/src/post/Post.jsx
+++ blog-app/src/post/Post.jsx
@@ -3,7 +3,40 @@ import "./post.css"
 export default function Post() {
   return (
     <div className="post">
-        post
+
+        <img className="postImg" src="https://cdn.animenewsnetwork.com/hotlink/thumbnails/max700x700/cms/news.6/195871/marriage.jpg" alt="" />
+
+        <div className="postInfo">
+            <div className="postCats">
+                <span className="postCat">Anime</span>
+                <span className="postCat">Entertainment</span>
+            </div>
+            <span className="postTitle">
+                Lorem ipsum dolor sit amet.
+            </span>
+            <hr />
+            <span className="postData">1 hours ago</span>
+        </div>
+        <p className="postDesc">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugiat dolores magnam, voluptas ad culpa, velit ut, 
+            dolor officiis possimus iusto illo error dolorum sapiente 
+            voluptatum dicta corrupti sit deserunt totam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugiat dolores magnam, voluptas ad culpa, velit ut, 
+            dolor officiis possimus iusto illo error dolorum sapiente 
+            voluptatum dicta corrupti sit deserunt totam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugiat dolores magnam, voluptas ad culpa, velit ut, 
+            dolor officiis possimus iusto illo error dolorum sapiente 
+            voluptatum dicta corrupti sit deserunt totam!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Fugiat dolores magnam, voluptas ad culpa, velit ut, 
+            dolor officiis possimus iusto illo error dolorum sapiente 
+            voluptatum dicta corrupti sit deserunt totam!
+        </p>
     </div>
+
+    
   )
 }
---
+++ blog-app/src/posts/Posts.jsx
@@ -0,0 +1,15 @@
+import "./posts.css"
+
+export default function posts() {
+  return (
+    <div className="posts">
+        <post/>
+        <post/>
+        <post/>
+        <post/>
+        <post/>
+        <post/>
+
+    </div>
+  )
+}
