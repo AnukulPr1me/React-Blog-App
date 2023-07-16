4a21bbaddce75996cad3f58df79dc7d85f6ec1fb
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-16 22:14:00 +0530

    1.7 update

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import TopBar from './components/TopBar/TopBar'
 import Home from './pages/home/Home';
+import Single from './pages/single/Single';
 
 function App() {
   return (
     <> 
       <TopBar/>
 
-      <Home/>
+      <Single/>
     </>
   );
 }
---
+++ blog-app/src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,44 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+        <div className="singlePostWrapper">
+            <img className="singlePostImg" 
+                src="https://cdn.animenewsnetwork.com/thumbnails/max600x600/cms/news.6/200315/mahouka_visual.jpg" alt="" 
+            />
+            <h1 className="singlePostTitle">
+                Lorem ipsum dolor sit amet consectetur.
+                <div className="singlePostEdit">
+                  <i className="singlePostIcon far fa-edit"></i>
+                  <i className=" singlePostIcon far fa-trash-alt"></i>
+                </div>
+            </h1>
+            <div className="singlePostInfo">
+              <span className="singlePostAuthor">Author: <b>Anukul Prime</b></span>
+              <span className="singlePostTime">1 hour ago</span>
+            </div>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, 
+              beatae explicabo! Sapiente facilis ducimus odit labore, 
+              sunt harum ea minus quo quisquam atque reiciendis aperiam, 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, 
+              beatae explicabo! Sapiente facilis ducimus odit labore, 
+              sunt harum ea minus quo quisquam atque reiciendis aperiam, 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, 
+              beatae explicabo! Sapiente facilis ducimus odit labore, 
+              sunt harum ea minus quo quisquam atque reiciendis aperiam, 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, 
+              beatae explicabo! Sapiente facilis ducimus odit labore, 
+              sunt harum ea minus quo quisquam atque reiciendis aperiam, 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, 
+              beatae explicabo! Sapiente facilis ducimus odit labore, 
+              sunt harum ea minus quo quisquam atque reiciendis aperiam, 
+              
+            </p>
+        </div>
+
+    </div>
+  )
+}
