1b28428307c05632e17f3928dd36bbe228b380e2
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-20 11:24:25 +0530

    blog writing format page

--- blog-app/src/pages/write/Write.jsx
+++ blog-app/src/pages/write/Write.jsx
@@ -3,14 +3,25 @@ import "./write.css"
 export default function Write() {
   return (
     <div className="write">
+      <img 
+        className="writeImg"
+        src="" alt="" 
+      />
         <form className="writeForm">
             <div className="writeFormGroup">
                 <label htmlFor="fileInput">
-                    <i className="fas fa-plus"></i>
+                    <i className=" writeIcon fas fa-plus"></i>
                 </label>
                 <input type="file" id="fileInput" style={{display: "none"}} />
                 <input type="text" placeholder="Title" className="writeInput" autoFocus = {true} />
             </div>
+            <div className="writeFormGroup">
+              <textarea placeholder="tell your story..." type ="text"className="writeInput">
+                
+              </textarea>
+            </div>
+
+            <button className="writeSubmit">Publish</button>
         </form>
     </div>
   )
