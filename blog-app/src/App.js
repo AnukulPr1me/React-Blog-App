290b384b634dffc22e33eb540aadd05338871957
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-22 22:57:50 +0530

    adding links

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,14 +1,54 @@
 
 import './App.css';
-import TopBar from './components/TopBar/TopBar'
+import TopBar from './components/TopBar/TopBar';
 import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Post from './components/post/Post';
+import Single from './pages/single/Single';
+
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 function App() {
+  const user = false;
   return (
-    <> 
+    <Router> 
+
       <TopBar/>
 
-      <Register/>
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route path="/register">
+          {user ? <Home/> :<Register/>}
+        </Route>
+
+        <Route path="/write">
+          {user ? <Write/> :<Register/>}
+        </Route>
+    
+
+        <Route path="/login">
+          {user ? <Home/> :<Login/>}
+        </Route>
+
+        <Route path="/post/:postId">
+          <Single/>
+        </Route>
+
+        <Route path="/settings">
+          {user ? <Settings/> :<Register/>}
+        </Route>
+    
+    
+      
+      </Switch>
+
+      
+    </Router>
   );
 }
 
--- blog-app/src/components/TopBar/TopBar.jsx
+++ blog-app/src/components/TopBar/TopBar.jsx
@@ -1,6 +1,9 @@
 import "./topbar.css"
+import { Link } from "react-router-dom";
+
 
 export default function TopBar() {
+    const user = false;
     return(
         <div className="top">
             <div className="topLeft">
@@ -12,15 +15,32 @@ export default function TopBar() {
 
             <div className="topCenter">
                 <ul className="topList">
-                    <li className="topListItem">HOME</li>
-                    <li className="topListItem">WRITE</li>
-                    <li className="topListItem">ABOUT</li>
-                    <li className="topListItem">CONTACT</li>
-                    <li className="topListItem">LOGOUT</li>
+                    <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
+                    <li className="topListItem"><Link className="link" to="/">WRITE</Link></li>
+                    <li className="topListItem"><Link className="link" to="/">ABOUT</Link></li>
+                    <li className="topListItem"><Link className="link" to="/">CONTACT</Link></li>
+                    <li className="topListItem"><Link className="link" to="/">LOGOUT</Link></li>
                 </ul>
             </div>
             <div className="topRight">
-                <img className="topImg" src="https://avatarfiles.alphacoders.com/106/106816.gif" alt="" />
+                {
+                    user ? (
+                            <img className="topImg" src="https://avatarfiles.alphacoders.com/106/106816.gif" alt="" />
+                        ) : (
+                            <ul className="topList">
+                                <li className="topListItem">
+                                    <Link className ="link" to="/login"> LOGIN</Link>
+
+                                    <Link className ="link" to="/register"> REGISTER</Link>
+
+                                </li>
+
+
+                            </ul>
+                        
+                        )
+                }
+               
 
                 <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
             </div>
